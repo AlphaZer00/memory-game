4b59082b585ca1fb8d56646236a97eb96397c5bd
AlphaZer00 <batudasdemir@gmail.com>
2025-01-08 12:15:32 -0500

    Change Scoreboard background color

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -1,6 +1,6 @@
 const Scoreboard = ({ currentScore, highestScore }) => {
     return (
-        <div className="bg-gray-800 text-white p-4 rounded-lg shadow-md flex justify-between items-center">
+        <div className="bg-black bg-opacity-50 text-white p-4 rounded-lg shadow-md flex justify-between items-center mb-2 w-[300px]">
             <div className="text-xl font-bold">Score: {currentScore}</div>
             <div className="text-xl font-bold">High Score: {highestScore}</div>
         </div>
